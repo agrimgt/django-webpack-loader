9f10e01bbe2e377ca5ac687c3ab1d30aed252a83
Phillip Marshall <wizpig64@users.noreply.github.com>
2018-09-11 18:30:15 -0700

    fixed bug when chunk names end with '?<hash>' after .js or .css

--- webpack_loader/utils.py
+++ webpack_loader/utils.py
@@ -62,11 +62,12 @@ def get_as_tags(bundle_name, extension=None, config='DEFAULT', attrs=''):
     bundle = _get_bundle(bundle_name, extension, config)
     tags = []
     for chunk in bundle:
-        if chunk['name'].endswith(('.js', '.js.gz')):
+        chunk_name = chunk['name'].split('?')[0]
+        if chunk_name.endswith(('.js', '.js.gz')):
             tags.append((
                 '<script type="text/javascript" src="{0}" {1}></script>'
             ).format(chunk['url'], attrs))
-        elif chunk['name'].endswith(('.css', '.css.gz')):
+        elif chunk_name.endswith(('.css', '.css.gz')):
             tags.append((
                 '<link type="text/css" href="{0}" rel="stylesheet" {1}/>'
             ).format(chunk['url'], attrs))
